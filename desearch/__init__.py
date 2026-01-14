1496b38fadb408487a7855efbd7ed695c1db4edd
Mirian Okradze <okradzemirian@gmail.com>
2026-01-14 15:03:35 +0400

    build: bump version

--- desearch/__init__.py
+++ desearch/__init__.py
@@ -19,7 +19,7 @@
 
 
 # version must stay on line 22
-__version__ = "0.0.197"
+__version__ = "0.0.198"
 version_split = __version__.split(".")
 __spec_version__ = (
     (1000 * int(version_split[0]))
