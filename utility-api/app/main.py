0169eddf60f8ff229c5771625e9a8a8e0f07dd1b
Mirian Okradze <okradzemirian@gmail.com>
2026-03-17 01:03:50 +0400

    feat: add CORS middleware to allow cross-origin requests

--- utility-api/app/main.py
+++ utility-api/app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.requests import Request
 
 from app.config import NETUID, SUBTENSOR_NETWORK
@@ -37,6 +38,13 @@ app = FastAPI(
     description="Subnet-22 (Desearch) dataset & logging utility API",
     version="0.1.0",
     lifespan=lifespan,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(dataset_router)
